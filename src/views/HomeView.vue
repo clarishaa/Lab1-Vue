8fe2c0e144eba458f7872cb427636c2f59b58a99
clarishaa <jabonillo.clarish@minsu.edu.ph>
2023-10-12 22:33:07 +0800

    .
    Co-Authored-By: Czairus <130778271+Czairus@users.noreply.github.com>
    Co-Authored-By: PrincessManibo <145077436+PrincessManibo@users.noreply.github.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -59,7 +59,7 @@
             <strong class="product-price">$78.00</strong>
 
             <span class="icon-cross">
-              <img src="../asstes/images/cross.svg" class="img-fluid">
+              <img :src="require('../assets/images/cross.svg')" class="img-fluid">
             </span>
           </a>
         </div>
@@ -68,12 +68,12 @@
         <!-- Start Column 4 -->
         <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
           <a class="product-item" href="#cart">
-            <img src="../asstes/images/product-3.png" class="img-fluid product-thumbnail">
+            <img :src="require('../assets/images/product-3.png')" class="img-fluid product-thumbnail">
             <h3 class="product-title">Ergonomic Chair</h3>
             <strong class="product-price">$43.00</strong>
 
             <span class="icon-cross">
-              <img src="../asstes/images/cross.svg" class="img-fluid">
+              <img :src="require('../assets/images/cross.svg')" class="img-fluid">
             </span>
           </a>
         </div>
@@ -97,7 +97,7 @@
             <div class="col-6 col-md-6">
               <div class="feature">
                 <div class="icon">
-                  <img src="../asstes/images/truck.svg" alt="Image" class="imf-fluid">
+                  <img :src="require('../assets/images/truck.svg')" alt="Image" class="imf-fluid">
                 </div>
                 <h3>Fast &amp; Free Shipping</h3>
                 <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
@@ -107,7 +107,7 @@
             <div class="col-6 col-md-6">
               <div class="feature">
                 <div class="icon">
-                  <img src="../asstes/images/bag.svg" alt="Image" class="imf-fluid">
+                  <img :src="require('../assets/images/bag.svg')" alt="Image" class="imf-fluid">
                 </div>
                 <h3>Easy to Shop</h3>
                 <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
@@ -117,7 +117,7 @@
             <div class="col-6 col-md-6">
               <div class="feature">
                 <div class="icon">
-                  <img src="../asstes/images/support.svg" alt="Image" class="imf-fluid">
+                  <img :src="require('../assets/images/support.svg')" alt="Image" class="imf-fluid">
                 </div>
                 <h3>24/7 Support</h3>
                 <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
@@ -127,7 +127,7 @@
             <div class="col-6 col-md-6">
               <div class="feature">
                 <div class="icon">
-                  <img src="../asstes/images/return.svg" alt="Image" class="imf-fluid">
+                  <img :src="require('../assets/images/return.svg')" alt="Image" class="imf-fluid">
                 </div>
                 <h3>Hassle Free Returns</h3>
                 <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
@@ -139,7 +139,7 @@
 
         <div class="col-lg-5">
           <div class="img-wrap">
-            <img src="../asstes/images/why-choose-us-img.jpg" alt="Image" class="img-fluid">
+            <img :src="require('../assets/images/why-choose-us-img.jpg')" alt="Image" class="img-fluid">
           </div>
         </div>
 
@@ -154,9 +154,9 @@
       <div class="row justify-content-between">
         <div class="col-lg-7 mb-5 mb-lg-0">
           <div class="imgs-grid">
-            <div class="grid grid-1"><img src="../asstes/images/img-grid-1.jpg" alt="Untree.co"></div>
-            <div class="grid grid-2"><img src="../asstes/images/img-grid-2.jpg" alt="Untree.co"></div>
-            <div class="grid grid-3"><img src="../asstes/images/img-grid-3.jpg" alt="Untree.co"></div>
+            <div class="grid grid-1"><img :src="require('../assets/images/img-grid-1.jpg')" alt="Untree.co"></div>
+            <div class="grid grid-2"><img :src="require('../assets/images/img-grid-2.jpg')" alt="Untree.co"></div>
+            <div class="grid grid-3"><img :src="require('../assets/images/img-grid-3.jpg')" alt="Untree.co"></div>
           </div>
         </div>
         <div class="col-lg-5 ps-lg-5">
@@ -186,7 +186,7 @@
         <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
           <div class="product-item-sm d-flex">
             <div class="thumbnail">
-              <img src="../asstes/images/product-1.png" alt="Image" class="img-fluid">
+              <img :src="require('../assets/images/product-1.png')" alt="Image" class="img-fluid">
             </div>
             <div class="pt-3">
               <h3>Nordic Chair</h3>
@@ -199,7 +199,7 @@
         <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
           <div class="product-item-sm d-flex">
             <div class="thumbnail">
-              <img src="../asstes/images/product-2.png" alt="Image" class="img-fluid">
+              <img :src="require('../assets/images/product-2.png')" alt="Image" class="img-fluid">
             </div>
             <div class="pt-3">
               <h3>Kruzo Aero Chair</h3>
@@ -212,7 +212,7 @@
         <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
           <div class="product-item-sm d-flex">
             <div class="thumbnail">
-              <img src="../asstes/images/product-3.png" alt="Image" class="img-fluid">
+              <img :src="require('../assets/images/product-3.png')" alt="Image" class="img-fluid">
             </div>
             <div class="pt-3">
               <h3>Ergonomic Chair</h3>
@@ -261,7 +261,7 @@
 
                       <div class="author-info">
                         <div class="author-pic">
-                          <img src="../asstes/images/person-1.png" alt="Maria Jones" class="img-fluid">
+                          <img :src="require('../assets/images/person-1.png'" alt="Maria Jones" class="img-fluid">
                         </div>
                         <h3 class="font-weight-bold">Maria Jones</h3>
                         <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
@@ -287,7 +287,7 @@
 
                       <div class="author-info">
                         <div class="author-pic">
-                          <img src="../asstes/images/person-1.png" alt="Maria Jones" class="img-fluid">
+                          <img :src="require('../assets/images/person-1.png')" alt="Maria Jones" class="img-fluid">
                         </div>
                         <h3 class="font-weight-bold">Maria Jones</h3>
                         <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
@@ -313,7 +313,7 @@
 
                       <div class="author-info">
                         <div class="author-pic">
-                          <img src="../asstes/images/person-1.png" alt="Maria Jones" class="img-fluid">
+                          <img :src="require('../assets/images/person-1.png')" alt="Maria Jones" class="img-fluid">
                         </div>
                         <h3 class="font-weight-bold">Maria Jones</h3>
                         <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
@@ -350,7 +350,7 @@
 
         <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
           <div class="post-entry">
-            <a href="#" class="post-thumbnail"><img src="../asstes/images/post-1.jpg" alt="Image" class="img-fluid"></a>
+            <a href="#" class="post-thumbnail"><img :src="require('../assets/images/post-1.jpg')" alt="Image" class="img-fluid"></a>
             <div class="post-content-entry">
               <h3><a href="#">First Time Home Owner Ideas</a></h3>
               <div class="meta">
@@ -362,7 +362,7 @@
 
         <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
           <div class="post-entry">
-            <a href="#" class="post-thumbnail"><img src="../asstes/images/post-2.jpg" alt="Image" class="img-fluid"></a>
+            <a href="#" class="post-thumbnail"><img :src="require('../assets/images/post-2.jpg')" alt="Image" class="img-fluid"></a>
             <div class="post-content-entry">
               <h3><a href="#">How To Keep Your Furniture Clean</a></h3>
               <div class="meta">
@@ -374,7 +374,7 @@
 
         <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
           <div class="post-entry">
-            <a href="#" class="post-thumbnail"><img src="../asstes/images/post-3.jpg" alt="Image" class="img-fluid"></a>
+            <a href="#" class="post-thumbnail"><img :src="require('../assets/images/post-3.jpg')" alt="Image" class="img-fluid"></a>
             <div class="post-content-entry">
               <h3><a href="#">Small Space Furniture Apartment Ideas</a></h3>
               <div class="meta">
@@ -394,13 +394,13 @@
     <div class="container relative">
 
       <div class="sofa-img">
-        <img src="../asstes/images/sofa.png" alt="Image" class="img-fluid">
+        <img :src="require('../assets/images/sofa.png')" alt="Image" class="img-fluid">
       </div>
 
       <div class="row">
         <div class="col-lg-8">
           <div class="subscription-form">
-            <h3 class="d-flex align-items-center"><span class="me-1"><img src="../asstes/images/envelope-outline.svg" alt="Image"
+            <h3 class="d-flex align-items-center"><span class="me-1"><img :src="require('../assets/images/envelope-outline.svg')" alt="Image"
                   class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
 
             <form action="#" class="row g-3">
